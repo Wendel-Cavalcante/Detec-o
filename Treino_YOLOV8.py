4be16455df9792fdd7b7cfefa53939caff3864fd
Wendel Cavalcante <150285896+Wendel-Cavalcante@users.noreply.github.com>
2024-04-19 08:55:55 -0300

    Add files via upload

---
+++ Treino_YOLOV8.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+
+# para marcar as imagens
+# https://www.makesense.ai/
+
+def main():
+    # Load a model
+    #model = YOLO("yolov8n.yaml")  # build a new model from scratch
+    model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
+
+    # Use the model
+    model.train(data="Pessoas.yaml", epochs=100, device=0, batch=5, imgsz=640, patience=100)  # train the model
+    metrics = model.val()  # evaluate model performance on the validation set
+    # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+    # path = model.export(format="onnx")  # export the model to ONNX format
+    # print("path", path)
+
+
+if __name__ == '__main__':
+    main()
